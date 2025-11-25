298d6a4dc44fec863f438edad231bc115b48fe19
sagar sharma <sagarkumarsharma546@proton.me>
2025-11-26 00:39:19 +0530

    Remove README

--- App.tsx
+++
@@ -1,3 +0,0 @@
-// This file is no longer used.
-// All code has been moved to index.tsx to allow for drag-and-drop deployment on Netlify
-// without requiring a build step for module imports.
--- main.js
+++
@@ -1,3 +0,0 @@
-// This application has been converted to a static website.
-// This file (main.js) is no longer used.
-// To run the app, simply serve index.html via any web server (e.g., 'npx serve .').
